a796bb0bc65bfaea979cd42a82758c58920c5357
Mike H <mhernandezit@gmail.com>
2021-03-10 08:33:00 -0500

    PEP8, gitignore

--- week1/convert.py
+++ week1/convert.py
@@ -5,8 +5,8 @@ import os
 current_dir = os.getcwd()
 source_dir = current_dir + '/images/'
 tar_dir = "/opt/icons/"
-#photo_list = os.listdir(source_dir)
-#print(photo_list)
+# photo_list = os.listdir(source_dir)
+# print(photo_list)
 
 if not os.path.exists(tar_dir):
     os.makedirs(tar_dir)
@@ -14,5 +14,5 @@ if not os.path.exists(tar_dir):
 for file in os.listdir(source_dir):
     if not file.startswith('.'):
         target = Image.open(source_dir + file)
-        target.convert('RGB').rotate(270).resize((128,128)).save(tar_dir + file, "JPEG")
+        target.convert('RGB').rotate(270).resize((128, 128)).save(tar_dir + file, "JPEG")
         target.close()
